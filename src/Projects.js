e5b50d1ae7227b5750be87efcb2ecb9bdcb021ce
JAYAPRAKASH0649 <mailtojayaprakashteek@gmail.com>
2024-07-18 18:44:03 +0530

    projects updated

--- src/Projects.js
+++ src/Projects.js
@@ -1,6 +1,10 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+import './project.css'
 const Projects = () => {
+  AOS.init()
   return (
     <div>
       <div className='navBar'>
@@ -11,6 +15,16 @@ const Projects = () => {
           <li><Link to="/projects" >Projects</Link></li>
           <li><Link to="/contact" >contact</Link></li>
         </ol>
+        <>
+        <div className='project 1'  >
+          <div className='links' data-aos="fade-left">
+            <a href='https://github.com/JAYAPRAKASHR0649/smallCommerceSite'>Amazon Look-alike E-commerce site</a>
+          </div>
+          <div className='links' data-aos="fade-left" data-aos-duration="1000" >
+            <a href='https://github.com/JAYAPRAKASHR0649/game'>Number Guessing Game</a>
+          </div>
+        </div>
+        </>
       </div>
       <div></div>
     </div>
